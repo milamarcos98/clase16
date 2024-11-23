852b6637227ab8b9c26767e6b5ed1058f5810d04
mila <milamarcos98@gmail.com>
2024-11-23 01:26:03 -0300

    code

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import { Home } from './pages'
+import { Route, Routes } from 'react-router-dom'
+import { Register } from './pages/Register/Register'
+import { Login } from './pages/Login/Login'
+import ProtectedRoutes from './utils/ProtectedRoutes'
+
+function App() {
+
+  return (
+    <>
+    <Routes>
+      <Route element={<ProtectedRoutes/>}>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<Home/>}/>
+      </Route>
+
+      <Route path="/register" element={<Register/>}/>
+      <Route path="/login" element={<Login/>}/>
+    </Routes>
+    </>
+  )
+}
+
+export default App
