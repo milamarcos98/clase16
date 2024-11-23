20814b63b701b62ede30fb69734e71ba0eee4f43
mila <milamarcos98@gmail.com>
2024-11-23 01:56:47 -0300

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { Home } from './pages'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, NavLink } from 'react-router-dom'
 import { Register } from './pages/Register/Register'
 import { Login } from './pages/Login/Login'
 import ProtectedRoutes from './utils/ProtectedRoutes'
@@ -9,6 +9,13 @@ function App() {
 
   return (
     <>
+    <nav>
+      <ul>
+        <li><NavLink to="/home">Home</NavLink></li>
+        <li><NavLink to="/register">Register</NavLink></li>
+        <li><NavLink to="/login">Login</NavLink></li>
+      </ul>
+    </nav>
     <Routes>
       <Route element={<ProtectedRoutes/>}>
         <Route path="/about" element={<Home/>}/>
